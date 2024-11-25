accba0a9d9d3af27bbdf5e3a4383e01a6709f8c3
u2pitchjami <u2pitchjami@gmail.com>
2024-11-25 12:53:08 +0100

    gitignore

--- recap_music.sh
+++ recap_music.sh
@@ -13,7 +13,8 @@
 #									                                         #
 ############################################################################## 
 #définition des variables
-source ./.config.cfg
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source ${SCRIPT_DIR}/.config.cfg
 
 if [ ! -d $DOSSIERLOGS ];then
 echo "Création du dossier Logs";
@@ -21,25 +22,25 @@ mkdir $DOSSIERLOGS
 fi
 
 touch "$LOG"
-cat "$ACHECKER" > TEMP
+cat "$ACHECKER" > ${SCRIPT_DIR}/TEMP
 #liste des fichiers modifiés sur les dernières 24h
 echo "Liste des fichiers modifiés sur les dernières 24h (via find)" | tee -a "$LOG"
-find ${COLLECTION} -maxdepth 3 -type f -mtime -1 \( -iname "*.flac" -o -iname "*.mp3" \) | cut -d'/' -f1-7 | uniq | tee -a "$LOG" | tee -a TEMP
-
+find ${COLLECTION} -maxdepth 3 -type f -mtime -1 \( -iname "*.flac" -o -iname "*.mp3" \) | cut -d'/' -f1-7 | uniq | tee -a "$LOG" | tee -a ${SCRIPT_DIR}/${SCRIPT_DIR}/TEMP
+#find ${COLLECTION} -maxdepth 3 -type f -mmin -60 \( -iname "*.flac" -o -iname "*.mp3" \) | cut -d'/' -f1-7 | uniq | tee -a "$LOG" | tee -a ${SCRIPT_DIR}/TEMP
 find ${COLLECTION} -maxdepth 2 -mindepth 2 -type d > ${DOSSIER}liste2.txt
 
 #dossiers ajoutés
 echo "Artistes et albums créés ou modifiés (comparaison de listes)" | tee -a "$LOG"
-diff -u ${DOSSIER}liste1.txt ${DOSSIER}liste2.txt | grep '^-' | sed 's/^-//' | tee -a "$LOG" | tee -a TEMP
+diff -u ${DOSSIER}liste1.txt ${DOSSIER}liste2.txt | grep '^-' | sed 's/^-//' | tee -a "$LOG" | tee -a ${SCRIPT_DIR}/TEMP
 
 #dossiers supprimés
 echo "Artistes et albums supprimés ou modifiés (comparaison de listes)" | tee -a "$LOG"
-diff -u ${DOSSIER}liste1.txt ${DOSSIER}liste2.txt | grep '^+' | sed 's/^+//' | tee -a "$LOG" | tee -a TEMP
+diff -u ${DOSSIER}liste1.txt ${DOSSIER}liste2.txt | grep '^+' | sed 's/^+//' | tee -a "$LOG" | tee -a ${SCRIPT_DIR}/TEMP
 
-sed -i "/liste1.txt/d" TEMP
-sed -i "/liste2.txt/d" TEMP
-sort TEMP | uniq > "$ACHECKER"
+sed -i "/liste1.txt/d" ${SCRIPT_DIR}/TEMP
+sed -i "/liste2.txt/d" ${SCRIPT_DIR}/TEMP
+sort ${SCRIPT_DIR}/TEMP | uniq > "$ACHECKER"
 
 rm ${DOSSIER}liste1.txt
 mv ${DOSSIER}liste2.txt ${DOSSIER}liste1.txt
-rm TEMP
+rm ${SCRIPT_DIR}/TEMP
